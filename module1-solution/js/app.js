432c19a27159f6924f2e7d9ed5fc7a0c86de99ed
Ralin Chimev <ralin.chimev@gmail.com>
2016-09-17 23:50:00 +0300

    Implement assignment 1 bonus

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -11,17 +11,25 @@
     $scope.checkMeals = function() {
       if (!$scope.mealsString) {
         $scope.message = "Please enter data first";
+        $scope.messageType = "danger";
       } else {
         var mealsCount = getMealsCount($scope.mealsString);
 
         if (mealsCount > 3) {
           $scope.message = "Too much!";
+          $scope.messageType = "success";
         } else if (mealsCount > 0) {
           $scope.message = "Enjoy!";
+          $scope.messageType = "success";
         } else {
           $scope.message = "Empty meals are not considered";
+          $scope.messageType = "danger";
         }
       }
+    };
+
+    $scope.getMessageClass = function() {
+      return 'alert alert-' + $scope.messageType;
     };
   }
 
